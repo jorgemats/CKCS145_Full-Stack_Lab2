b9fa97e87262e9e9161e3fff0d767d11368c6266
jorge.m <matsoukas.george@gmail.com>
2023-01-27 14:59:47 -0500

    Added call to test.html in second flask app route and corrected test.html syntax error

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
+#all routes need to be below this app variable declaration
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,7 +13,9 @@ def param_home():
 	
 @app.route('/param/<name>')
 def param_submit(name):
-	return 'Parameter %s!' % name
+	return render_template('test.html', name=name)	#Added call to html template
+	#return 'Parameter %s!' % name	#comment out this line when using template
 
+#all routes need to be above this if statement
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='5090')
